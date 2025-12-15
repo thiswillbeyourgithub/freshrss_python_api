9681561961e932b4fe68fc8d6cb0ee9ddec6d283
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-12-15 19:38:03 +0100

    bump version 2.0.4 -> 2.0.5

--- freshrss_api/freshrss_api.py
+++ freshrss_api/freshrss_api.py
@@ -26,7 +26,7 @@ from .utils.errors import APIError, AuthenticationError
 class FreshRSSAPI:
     """A Python client for the FreshRSS Fever API."""
 
-    VERSION: str = "2.0.4"
+    VERSION: str = "2.0.5"
 
     def __init__(
         self,
